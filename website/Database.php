5f40ed9781f4f8ec9a2a67b630dfce8236e79bb6
AndySong <andylauszp@gmail.com>
2023-02-09 01:22:03 +1300

    env-vars

--- website/Database.php
+++ website/Database.php
@@ -4,11 +4,14 @@ class Database
 {
     private PDO $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-        $this->connection = new PDO($dsn);
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query): PDOStatement
--- website/index.php
+++ website/index.php
@@ -4,9 +4,10 @@ require_once 'functions.php';
 require_once 'Database.php';
 
 //require_once 'router.php';
+$config = require_once 'config.php';
 
-$db = new Database();
-$posts = $db->query("select * from posts where id = 1")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+$posts = $db->query("select * from posts where id = 1")->fetchAll();
 
 foreach ($posts as $post) {
     echo "<li>{$post['title']}</li>";
