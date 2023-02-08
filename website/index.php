196bffd9e671d7c43c04ce33705f824e455082e3
AndySong <andylauszp@gmail.com>
2023-02-08 18:06:30 +1300

    PDO

--- website/index.php
+++ website/index.php
@@ -2,4 +2,18 @@
 
 require_once 'functions.php';
 
-require_once 'router.php';
+//require_once 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
